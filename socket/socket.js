b3be434d60ad9e3af92dd2b442db0a70bfb73b32
ishuacharis <ishuacharis@gmail.com>
2021-02-10 10:34:55 +0100

    removed the comments

--- socket/socket.js
+++ socket/socket.js
@@ -1,19 +1,3 @@
-// const socket_io = require("socket.io");
-// const io = socket_io();
-// const socket = {};
-// socket.io = io;
-
-// io.on('connection', async function(socket) {
-
-//     console.log("connected to socket")
-// })
-
-// io.on('disconnect', function() {
-//     console.log("socket disconnected")
-// })
-
-// module.exports = socket;
-
 module.exports = function(server) {
     const io = require("socket.io")(server, {
 
