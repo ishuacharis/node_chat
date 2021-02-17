a221e99633d05131cdccec2b9ce5e1d8989d27af
ishuacharis <ishuacharis@gmail.com>
2021-02-17 02:00:25 +0100

    add use_typin and new_user event

--- socket/socket.js
+++ socket/socket.js
@@ -1,27 +1,51 @@
+const { PayloadTooLarge } = require("http-errors");
+
 module.exports = function(server) {
     const io = require("socket.io")(server, {
-
         cors: {
             origin: "http://localhost:8080",
             methods: ["GET", "POST"]
         }
-
     });
-
+    const users  = {};
+    const typers = [];
 
     io.on("connection", (socket) => {
-        console.log("socket connected on now ins socket file "+ socket.id)
+        console.log({
+            info: "socket connected on now ins socket file "+ socket.id,
+            id: socket.id
+        })
         io.emit("hello", "world")
 
-        socket.emit('isconnected', {
-            connected: 'successfull'
-        })
+    
         socket.on("typing" , (data, callback) => {
        
             console.log(`vue data ${data.isTyping}`)
 
             callback({
                 status: 'ok'
+            })
+        })
+
+        //listen to user typing
+        socket.on('user_typing' , () => {
+            typers[socket.id] = 1
+
+            socket.broadcast.emit("user_typing" , {
+                user: users[socket.id].username,
+                typers: Object.keys(typers).length
+            })
+        })
+
+        //listen to new user
+        socket.on('new_user', ({username}) => {
+            users[socket.id] =  {
+                id: socket.id,
+                username: username
+            }
+            console.log({
+                users: users,
+                totalusers: Object.keys(users).length
             })
         })
         
