9c26c0a958fb79c76e06f3af98ca2e2d4fa9624c
ishuacharis <ishuacharis@gmail.com>
2021-02-21 03:19:49 +0100

    import database options

--- config/server.js
+++ config/server.js
@@ -1,22 +1,18 @@
 const app = require("../app");
 const http = require('http');
 const database =  require('../database/connect');
-
+const OPTIONS = require("../database/database_options")
 const {
     PORT,
 } = process.env
 
 const port  =  PORT || '4000';
 const uri = "mongodb://localhost:27017/chatapp";
-const options = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-};
+
 app.set('port', port);
 const server  = http.createServer(app)
 const socket = require("../socket/socket")(server);
-database(uri, options)
+database(uri, OPTIONS);
 
 server.listen(PORT || 3000, function() {
     console.log(`server started ${PORT || 3000}`)
