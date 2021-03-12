a64fd90937af7fcc4dcea81efdcc7fce1a891c29
ishuacharis <ishuacharis@gmail.com>
2021-03-12 14:51:06 +0100

    remove password field from user model

--- models/user.js
+++ models/user.js
@@ -5,10 +5,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    password: {
-        type: String,
-        required: true
-    },
     phone_no: {
         type: String,
         required: true
